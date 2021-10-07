cf3c8aeea8ec3999d86a028712e693e7bb0152b4
Jaice561 <jameson_exil@yahoo.com>
2021-10-07 08:05:56 -0500

    CarDetail models and controllers and view

---
+++ JamoRental/Controllers/CarDetailsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JamoRental.Models;
+
+namespace JamoRental.Controllers
+{
+    public class CarDetailsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CarDetailsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();    
+        }
+
+
+        // GET: CarDetails
+        public ActionResult Index()
+        {
+            var carDetail = _context.CarDetails.ToList();
+
+            return View("CarDetails", carDetail);
+        }
+    }
+}
---
+++ JamoRental/Models/CarDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JamoRental.Models
+{
+    // The details of the cars
+    public class CarDetails
+    {
+        public int Id { get; set; }
+        public Double CarMileage { get; set; }
+        public string CarDiscription { get; set; }
+        public string CarColor { get; set; }
+    }
+}
